263494fab59cc2dd86d51788c2c3ddfec2dca2de
JGustavo11 <jgustavo11@mail.fresnostate.edu>
2016-03-06 20:38:51 -0800

    Added files via upload

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main (){
+
+	String := "Put your money where your mouth is"
+	number := 69
+	floatnumber := 69.69
+
+
+
+	fmt.Printf("%T \n", String)
+	fmt.Println(String)
+	fmt.Printf("%T \n", number)
+	fmt.Println(number)
+	fmt.Printf("%T \n", floatnumber)
+	fmt.Println(floatnumber)
+
+}
