2da286419467eaac83b4ba6dc2629cdb3155bc73
JGustavo11 <jgustavo11@mail.fresnostate.edu>
2016-03-13 16:37:09 -0700

    first commit

---
+++ Challenges/even/even.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("hmmmm")
+
+	for i := 0; i <= 100; i++ {
+
+		if i%2 == 0 {
+
+			fmt.Println(i)
+		}
+	}
+
+	fmt.Println("still hmmm")
+}
